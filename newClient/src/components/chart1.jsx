d9a20f978d73a1c6f418d23d7a6e8107babe63de
Matthew Goldwater <mattgwater@gmail.com>
2016-12-16 21:52:59 -0800

    Add legend to pie chart

---
+++ newClient/src/components/chart1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+   return (
+    ) 
+  }
+}
+
+module.exports = PieChart;
