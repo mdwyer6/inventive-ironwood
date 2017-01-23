7a963f1a41b86cce07a1c3c613dbbf7045f3a461
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-23 13:42:09 -0800

    Upgrade to webpack 2.0, implement treeshaking

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,29 @@ var config = {
       {
         test : /\.jsx?/,
         include : APP_DIR,
-        loader : 'babel'
+        loader : 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+    })
+  ]
 };
 
 module.exports = config;
